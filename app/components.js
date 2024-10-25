d662702580aff4e25220c86c34c215bb7c4c7fd3
Tanisha-philips <Tanisha.Aggarwal@philips.com>
2024-10-25 08:38:10 +0530

    Update components.js

--- app/components.js
+++ app/components.js
@@ -1,1 +1,15 @@
-ds
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+function ColorPair(){
+           this.majorColor;
+           this.minorColor;
+}
+
+ColorPair.prototype.toString=function(){
+	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
+}
