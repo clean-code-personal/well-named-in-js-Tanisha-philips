0cfa643bac272973b0ad3d144dbdc512a977be8b
Tanisha-philips <Tanisha.Aggarwal@philips.com>
2024-10-25 08:37:57 +0530

    Update colorCoder.js

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -1,23 +1,5 @@
 import getColorFromPairNumber from "./getColorFromPairNumber";
-
-const MajorColorNames = [
-    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
-];
-const MinorColorNames = [
-	"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
-];
-
-function ColorPair(){
-           this.majorColor;
-           this.minorColor;
-}
-
-ColorPair.prototype.toString=function(){
-	return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;
-}
-
-
-	 
+ 
 	 function test(){
 			pairNumber = 4;
             let testPair1 = getColorFromPairNumber(pairNumber);
