3974c5eaebf345a54f5d86b879c79d5e35d84e22
Tanisha-philips <Tanisha.Aggarwal@philips.com>
2024-10-28 09:27:18 +0530

    Create printColorCodeManual.js

---
+++ app/printColorCodeManual.js
@@ -0,0 +1,12 @@
+const { getColorFromPairNumber, getPairNumberFromColor } = require("./colorUtils");
+const ColorPair = require("./ColorPair");
+
+function printColorCodeManual() {
+    console.log("Color Code Reference Manual:");
+    console.log("Pair Number | Major Color | Minor Color");
+    console.log("----------------------------------------");
+    for (let pairNumber = 1; pairNumber <= 25; pairNumber++) {
+        const colorPair = getColorFromPairNumber(pairNumber);
+        console.log(`${pairNumber.toString().padEnd(11)} | ${colorPair.majorColor.padEnd(11)} | ${colorPair.minorColor}`);
+    }
+}
